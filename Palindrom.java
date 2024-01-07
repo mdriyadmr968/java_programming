445270ae29759118dd378b1e5d91f61879f7c773
syied <syied@onnftverse.com>
2024-01-08 02:16:20 +0600

    palindrome solved

---
+++ Palindrom.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Palindrom {
+
+    public static boolean isPalindrom(String str){
+        for(int i=0; i<str.length()/2; i++){
+            int n = str.length();
+            if(str.charAt(i) != str.charAt(n-1-i)){
+                return false;
+            }          
+
+        }
+         return true;
+    }
+    public static void main (String[] args){
+       
+        System.out.println(isPalindrom("noabcdon"));
+
+
+    }
+    
+}
